7f79c78f05648ae2858cae9a5a2b3714e6c52ca9
Jiahao <945770946@qq.com>
2022-02-07 11:24:21 +0800

    master commit2

--- src/main/java/com/jiahao/git/GitTest.java
+++ src/main/java/com/jiahao/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.printf("hello,git2!");
         System.out.printf("hello,git3!");
         System.out.printf("hello,git hot-fix!");
+        System.out.printf("master!");
     }
 }

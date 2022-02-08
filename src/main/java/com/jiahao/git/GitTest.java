3db49d4ec7329695e2fcf00c1b8ecedc88bef41d
Jiahao <945770946@qq.com>
2022-02-08 16:07:06 +0800

    push commit

--- src/main/java/com/jiahao/git/GitTest.java
+++ src/main/java/com/jiahao/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.printf("hello,git hot-fix!");
         System.out.printf("master!");
         System.out.printf("hot-fix!");
+        System.out.println("push commit");
     }
 }
